b5815c01985ad2a253572591cca3e09aa760298f
Alexandre <alexandrefaria3012@gmail.com>
2024-08-19 15:33:02 -0300

    Update index.js

--- src/componentes/Menu/index.js
+++ src/componentes/Menu/index.js
@@ -1,6 +1,21 @@
-const Menu = () => {
-    return(
+import { Link, useLocation } from "react-router-dom"
+import styles from "./Menu.module.css"
 
+const Menu = () => {
+
+    const localizacao = useLocation()
+
+    return (
+        <header>
+            <nav className={styles.navegacao}>
+                <Link className={styles.link} to="/">
+                    Inicio
+                </Link>
+                <Link className="link" to="/sobremim">
+                    SobreMim
+                </Link>
+            </nav>
+        </header>
     )
 }
 
