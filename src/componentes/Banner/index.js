067e739fe3e57fb57a966dfb3f5b6446f33f9382
Alexandre <alexandrefaria3012@gmail.com>
2024-08-20 15:22:00 -0300

    Update index.js

--- src/componentes/Banner/index.js
+++ src/componentes/Banner/index.js
@@ -10,15 +10,14 @@ const Banner = () =>{
 
                 <p className={styles.paragrafo}>
                     Olá, bem-vindo ao meu cantinho de projetos!
-                    Sou apaixonado por transformar ideias em códigos e dar vida a soluções criativas. Como desenvolvedor front-end, adoro explorar as infinitas possibilidades que a web oferece. Aqui, você vai encontrar alguns dos meus projetos, onde coloco em prática o que aprendo e desafio meus limites.
-
-                    Cada linha de código é uma nova oportunidade para evoluir, e eu estou sempre em busca de novos desafios. Sinta-se à vontade para explorar, dar sugestões ou até mesmo colaborar. Vamos construir algo incrível juntos!
+                    Sou apaixonado por transformar ideias em códigos e dar vida a soluções criativas. Como desenvolvedor front-end, adoro explorar as infinitas possibilidades que a web oferece. Aqui, você vai encontrar alguns dos meus projetos.
                 </p>
             </div>
 
             <div className={styles.imagens}>
                 <img className={styles.circuloColorido}
                     src={circuloColorido}
+                    alt='circulo'
                     aria-hidden= {true} 
                 />
 
