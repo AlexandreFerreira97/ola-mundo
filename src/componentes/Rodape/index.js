c46b969e159d02926a31c54c1bff054c958cfbb3
Alexandre <alexandrefaria3012@gmail.com>
2024-08-20 17:41:08 -0300

    Update index.js

--- src/componentes/Rodape/index.js
+++ src/componentes/Rodape/index.js
@@ -1,7 +1,13 @@
 import styles from './Rodape.module.css'
+import {ReactComponent as MarcaRegsitrada}  from '../assets/marca_registrada.svg'
 
 const Rodape = () => {
-    return ()
+    return (
+        <footer className={styles.rodape}>
+            <MarcaRegsitrada/>
+            Desenvolvido por Alexandre Ferreira
+        </footer>
+    )
 }
 
 export default Rodape
