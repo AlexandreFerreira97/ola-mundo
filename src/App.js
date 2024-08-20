5cb34d2f799a7d9d4ab3a214841cccde1076b6d8
Alexandre <alexandrefaria3012@gmail.com>
2024-08-20 17:41:06 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes,Route } from "react-router-dom";
 import Inicio from './paginas/Inicio'
 import SobreMim from './paginas/SobreMim'
 import Menu from "./componentes/Menu";
+import Rodape from "./componentes/Rodape";
 
 function App() {
   return (
@@ -14,6 +15,8 @@ function App() {
         <Route path="/SobreMim" element={<SobreMim/>}/>
         <Route path="*" element= {<div><h1>Page not found</h1></div>}/>
       </Routes>
+
+      <Rodape/>
     </BrowserRouter>
   );
 }
