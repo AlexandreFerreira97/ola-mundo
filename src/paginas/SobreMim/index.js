d49981f6ac7c8740cc713853fd6aef358552702d
Alexandre <alexandrefaria3012@gmail.com>
2024-08-21 16:19:21 -0300

    Update index.js

--- src/paginas/SobreMim/index.js
+++ src/paginas/SobreMim/index.js
@@ -1,6 +1,12 @@
+import PostModelo from "../../componentes/PostModelo"
+import fotoCapa from '../../componentes/assets/sobre_mim_capa.png'
+import styles from './SobreMim.module.css'
+
 const SobreMim = () => {
     return(
-        <h1>Sobre Mim</h1>
+        <PostModelo fotoCapa={fotoCapa} titulo="Sobre mim">
+            <h3 className={styles.subTitulo}>d</h3>
+        </PostModelo>
     )
 }
 
