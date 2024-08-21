71e90a5ed837f1f1b96180e58cfde227b6621113
Alexandre <alexandrefaria3012@gmail.com>
2024-08-21 16:19:22 -0300

    Update index.js

--- src/componentes/PostModelo/index.js
+++ src/componentes/PostModelo/index.js
@@ -1,7 +1,18 @@
-import './PostModelo.module.css'
+import styles from './PostModelo.module.css'
 
-const PostModelo = () => {
-    return()
+const PostModelo = ({children,fotoCapa,titulo}) => {
+    return(
+        <article className={styles.postModeloContainer}>
+            <div className={styles.fotoCapa} style={{backgroundImage: `url(${fotoCapa})`}}></div>
+
+            <h2 className={styles.titulo}>{titulo}</h2>
+
+            <div className={styles.postConteudoContainer}>
+                {children}
+            </div>
+
+        </article>
+    )
 }
 
 export default PostModelo
