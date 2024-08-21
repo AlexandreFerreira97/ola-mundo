e2140dc8efb7f53de3aff21d9fafc64a507f3b8e
Alexandre <alexandrefaria3012@gmail.com>
2024-08-21 16:56:20 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
         <Route path="/" element={<PaginaPadrao/>}>
           <Route path="/" element = {<Inicio/>}/>
           <Route path="/SobreMim" element={<SobreMim/>}/>
-          <Route path="/post" element={<Post/>}/>
+          <Route path="/posts/:id" element={<Post/>}/>
         </Route>
 
         <Route path="*" element= {<div><h1>Page not found</h1></div>}/>
