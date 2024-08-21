a654f1cae611dfbca3b72253286dc27d29b2690b
Alexandre <alexandrefaria3012@gmail.com>
2024-08-21 16:56:23 -0300

    Update index.js

--- src/componentes/PostCard/index.js
+++ src/componentes/PostCard/index.js
@@ -1,27 +1,23 @@
 import styles from './Post.module.css'
 import github from '../assets/github.png'
+import { Link } from 'react-router-dom'
 
-const PostCard = ( {post} ) => {
+const PostCard = ({ post }) => {
     return (
-        <div className={styles.post}>
-            <img className={styles.capa}
-                src={`/imgPR/posts/${post.id}/capa.PNG`}
-                alt='Imagem do Projeto'
-            />
+        <Link to={`/posts/${post.id}`}>
 
-            <h2 className={styles.titulo}>{post.titulo}</h2>
-
-            <button className={styles.botaoLer}>Descrição</button>
-
-            <a className={styles.ancora} href='https://github.com/AlexandreFerreira97'>
-                <img className={styles.gitH}
-                    src={github}
-                    alt='github'
-                    title='GitHub'
+            <div className={styles.post}>
+                <img className={styles.capa}
+                    src={`/imgPR/posts/${post.id}/capa.PNG`}
+                    alt='Imagem do Projeto'
                 />
-            </a>
 
-        </div>
+                <h2 className={styles.titulo}>{post.titulo}</h2>
+
+                <button className={styles.botaoLer}>Descrição</button>
+
+            </div>
+        </Link>
     )
 }
 
