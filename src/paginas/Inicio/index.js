af5a82b3ecb42be8b900c7e4fdfe83baf319ccab
Alexandre <alexandrefaria3012@gmail.com>
2024-08-21 16:02:52 -0300

    Update index.js

--- src/paginas/Inicio/index.js
+++ src/paginas/Inicio/index.js
@@ -1,23 +1,16 @@
-import Banner from "../../componentes/Banner"
 import styles from './Inicio.module.css'
 import posts from '../../componentes/json/posts.json'
 import Post from "../../componentes/Post"
 
 const Inicio = () => {
     return (
-
-        <main>
-            <Banner/>
-
-            <ul className={styles.pts}>
-                {posts.map( (post) => (
-                    <li key={post.id}>
-                        <Post post={post}/>
-                    </li>
-                ) )}
-            </ul>
-
-        </main>
+        <ul className={styles.pts}>
+            {posts.map((post) => (
+                <li key={post.id}>
+                    <Post post={post} />
+                </li>
+            ))}
+        </ul>
     )
 }
 
