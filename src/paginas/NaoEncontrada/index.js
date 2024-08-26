7af4f0b955b3c301075c8328ac73cfeec57dde8a
Alexandre <alexandrefaria3012@gmail.com>
2024-08-26 12:28:51 -0300

    Update index.js

--- src/paginas/NaoEncontrada/index.js
+++ src/paginas/NaoEncontrada/index.js
@@ -1,7 +1,23 @@
 import styles from './NaoEncontrada.module.css'
 
 const NaoEncontrada = () => {
+    return (
+        <div className={styles.conteudoContainer}>
 
+            <span className={styles.texto404}>404</span>
+            
+            <h1 className={styles.titulo}>Ops! Página não encontrada :(</h1>
+            
+            <p className={styles.paragrafo}>
+                Tem certeza que era isso que estava procurando?
+            </p>
+
+            <p className={styles.paragrafo}>
+                Aguarde um pouco e recarregue a página, ou volte para a página inicial.
+            </p>
+
+        </div>
+    )
 }
 
 export default NaoEncontrada
